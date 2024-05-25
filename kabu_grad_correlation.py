acf28b3417a66a14c42326687d9597a451a245e9
jsato0807 <63589838+jsato0807@users.noreply.github.com>
2024-05-25 13:47:06 +0900

    add kabu_grad_correlation.py and modified kabu_makegraph.py to adjust it

--- kabu_grad_correlation.py
+++ kabu_grad_correlation.py
@@ -1,32 +1,55 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from kabu_makegraph import makegraph
 
-def get_Log_Return_Diff(file_path,):
+
+def get_Log_Return_Diff(file_path):
     # エクセルファイルを読み込む
     df = pd.read_excel(file_path)
     return df['Log_Return_Diff']
 
+def get_corr(data,state):
 
+	df = pd.DataFrame(data)
 
-
-def main(code,year,month,period):
+	# 相関係数の計算
+	correlation = df.corr()
   
-  PRE_CORR = []
-  POST_CORR = []
-  for year in range(start_year,end_year+1):
-    code, year, month, period = input().split("-")
+	df["{}_corr".format(state)] = correlation
+	return df
 
-    file_path = "{}-{}-{}-{}-pre_grad_output.xlsx".format(code, year, month, period)
-    get_Log_Return_Diff("./xlsx_dir/{}".format(file_path), "pre_grad")
 
-    file_path = "{}-{}-{}-{}-post_grad_output.xlsx".format(code, year, month, period)
-    get_Log_Return_Diff("./xlsx_dir/{}".format(file_path), "post_grad")
 
-    # グラフを保存
-    plt.savefig('./png_dir/{}-{}-{}-{}_grad.png'.format(code, year, month, period))
-    print("Saved {}-{}-{}-{}_grad.png".format(code, year, month, period))
+
+
 
 if __name__ == "__main__":
     print("write code, start_year,end_year, month,period")
     code, start_year,end_year, month,period = input.split("-")
-    main(code,start_year,end_year,month,period)
+    
+    PRE_CORR = []
+    POST_CORR = []
+
+    for year in range(start_year,end_year+1):
+
+      file_path = "{}-{}-{}-{}-pre_grad_output.xlsx".format(code, year, month, period)
+      a = get_Log_Return_Diff(file_path)
+      PRE_CORR.append(a)
+
+      file_path = "{}-{}-{}-{}-post_grad_output.xlsx".format(code, year, month, period)
+      b = get_Log_Return_Diff(file_path)
+      POST_CORR.append(b)
+
+    fig, ax = plt.subplots(figsize=(15, 8))
+    
+    df = get_corr(PRE_CORR,"pre_corr")
+    y_label = "pre_corr"
+    makegraph(df,ax,"pre_corr")
+    
+    df = get_corr(POST_CORR,"post_corr")
+    y_label = "post_corr"
+    makegraph(df,ax,"post_corr")
+    
+    # グラフを保存
+    plt.savefig('./png_dir/{}-{}-{}-{}_corr.png'.format(code, year, month, period))
+    print("Saved {}-{}-{}-{}_corr.png".format(code, year, month, period))
--- kabu_makegraph.py
+++ kabu_makegraph.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def makegraph(file_path, ax, label):
-    # エクセルファイルを読み込む
-    df = pd.read_excel(file_path)
+def makegraph(df, ax, label,y_label):
 
     # グラフを作成
-    ax.plot(df["Date"], df["Log_Return_Diff"], label=label)
+    ax.plot(df["Date"], df["{}".format(y_label)], label=label)
 
     # グラフの装飾
     ax.set_title('Stock Prices Over Time')
     ax.set_xlabel('Date')
-    ax.set_ylabel('Log_Return_Diff')
+    ax.set_ylabel('{}'.format(y_label))
     ax.legend()
     ax.grid(True)
 
@@ -20,11 +18,18 @@ def main():
 
     code, year, month, period = input().split("-")
 
+    y_label = "Log_Return_Diff"
+
+
     file_path = "{}-{}-{}-{}-pre_grad_output.xlsx".format(code, year, month, period)
-    makegraph("./xlsx_dir/{}".format(file_path), ax, "pre_grad")
+        # エクセルファイルを読み込む
+    df = pd.read_excel("./xlsx_dir/{}".format(file_path))
+    makegraph(df, ax, "pre_grad",y_label)
 
     file_path = "{}-{}-{}-{}-post_grad_output.xlsx".format(code, year, month, period)
-    makegraph("./xlsx_dir/{}".format(file_path), ax, "post_grad")
+        # エクセルファイルを読み込む
+    df = pd.read_excel("./xlsx_dir/{}".format(file_path))
+    makegraph(df, ax, "post_grad",y_label)
 
     # グラフを保存
     plt.savefig('./png_dir/{}-{}-{}-{}_grad.png'.format(code, year, month, period))
