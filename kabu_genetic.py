99413aaf15de92496061095ae640644d842bc536
jsato0807 <63589838+jsato0807@users.noreply.github.com>
2024-08-24 06:00:33 +0900

    modified to wrap evaluate in genetic_algorithm_with_ct

--- kabu_genetic.py
+++ kabu_genetic.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.model_selection import KFold
 from kabu_swap import get_html, parse_swap_points, rename_swap_points, SwapCalculator
 from kabu_backtest import data, traripi_backtest, initial_funds, grid_end, grid_start, entry_intervals, total_thresholds, strategies
+from sklearn.preprocessing import OneHotEncoder
+from functools import partial
 
 entry_interval = entry_intervals[0]
 total_threshold = total_thresholds[0]
@@ -19,7 +21,6 @@ PARAM_BOUNDS = {
     'num_trap': (4, 101),
     'profit_width': (0.001, 100),
     'order_size': (1000, 10000),
-    'strategy_idx': tuple(i for i in range(len(strategies))),
     'density': (1.0, 10.0)
 }
 
@@ -27,18 +28,22 @@ PARAM_BOUNDS = {
 def custom_order_size():
     return random.choice(range(PARAM_BOUNDS['order_size'][0], PARAM_BOUNDS['order_size'][1] + 1, 1000))
 
+# ワンホットエンコーディングの準備
+encoder = OneHotEncoder(sparse=False)
+encoder.fit(np.array(strategies).reshape(-1, 1))
+
 # 適応度を評価する関数
 def evaluate(individual, data_subset):
-    num_trap, profit_width, order_size, density, strategy_idx = individual
-    num_trap = int(num_trap)
-    strategy_idx = int(strategy_idx)  # インデックスを整数に変換
-    if strategy_idx < 0 or strategy_idx >= len(strategies):
-        strategy_idx = random.randint(0, len(strategies) - 1)
-    strategy = strategies[strategy_idx]  # 整数から文字列に変換
+    num_trap, profit_width, order_size, density = individual[:4]
+    strategy_onehot = individual[4:]
+    strategy_idx = np.argmax(strategy_onehot)  # ワンホットエンコーディングをデコード
+    strategy = strategies[strategy_idx]  # インデックスから戦略を取得
+    
     if strategy != 'diamond':
         density = 1.0  # diamond以外の戦略ではdensityを固定値とする
+    
     effective_margin, _, _, _, _, _, _, _, _, _ = traripi_backtest(
-        calculator, data_subset, initial_funds, grid_start, grid_end, num_trap, profit_width, order_size, entry_interval, total_threshold, strategy, density
+        calculator, data_subset, initial_funds, grid_start, grid_end, int(num_trap), profit_width, order_size, entry_interval, total_threshold, strategy, density
     )
     
     return effective_margin,
@@ -53,12 +58,15 @@ toolbox = base.Toolbox()
 toolbox.register("num_trap", random.randint, *PARAM_BOUNDS['num_trap'])
 toolbox.register("profit_width", random.uniform, *PARAM_BOUNDS['profit_width'])
 toolbox.register("order_size", custom_order_size)
-toolbox.register("strategy_idx", random.choice, PARAM_BOUNDS['strategy_idx'])  # 修正箇所
 toolbox.register("density", random.uniform, *PARAM_BOUNDS['density'])
+
+# strategyのワンホットエンコーディング
+def random_strategy():
+    return encoder.transform([[random.choice(strategies)]])[0]
 
 # 個体と個体群を生成
 toolbox.register("individual", tools.initCycle, creator.Individual,
-                 (toolbox.num_trap, toolbox.profit_width, toolbox.order_size, toolbox.strategy_idx, toolbox.density), n=1)
+                 (toolbox.num_trap, toolbox.profit_width, toolbox.order_size, toolbox.density, random_strategy), n=1)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 # 適応度の評価、交叉、突然変異、選択を定義
@@ -67,13 +75,20 @@ toolbox.register("mate", tools.cxBlend, alpha=0.5)
 toolbox.register("mutate", tools.mutPolynomialBounded, low=[PARAM_BOUNDS['num_trap'][0], PARAM_BOUNDS['profit_width'][0], PARAM_BOUNDS['order_size'][0], PARAM_BOUNDS['density'][0]],
                  up=[PARAM_BOUNDS['num_trap'][1], PARAM_BOUNDS['profit_width'][1], PARAM_BOUNDS['order_size'][1], PARAM_BOUNDS['density'][1]], eta=0.2, indpb=0.2)
 
-# `strategy_idx` の突然変異は別途処理
+# `strategy` の突然変異は別途処理
 def mutate_individual(individual):
+    # num_trap, profit_width, order_size, density の部分に突然変異を適用
+    individual[:4] = tools.mutPolynomialBounded(individual[:4], 
+                                                low=[PARAM_BOUNDS['num_trap'][0], PARAM_BOUNDS['profit_width'][0], PARAM_BOUNDS['order_size'][0], PARAM_BOUNDS['density'][0]],
+                                                up=[PARAM_BOUNDS['num_trap'][1], PARAM_BOUNDS['profit_width'][1], PARAM_BOUNDS['order_size'][1], PARAM_BOUNDS['density'][1]],
+                                                eta=0.2, indpb=0.2)[0]
+    
+    # strategy の部分に突然変異を適用
     if random.random() < 0.2:
-        individual[3] = random.choice(PARAM_BOUNDS['strategy_idx'])  # `strategy_idx` の突然変異
-    # その他のパラメータの突然変異を個別に処理
-    toolbox.mutate([individual[i] for i in range(len(individual)) if i != 3])
+        individual[4:] = random_strategy()  # `strategy` の突然変異
+    
     return individual,
+
 
 toolbox.register("mutate", mutate_individual)
 toolbox.register("select", tools.selTournament, tournsize=3)
@@ -86,27 +101,33 @@ def genetic_algorithm_with_cv(data, k_folds=10):
         train_data = data[train_index]
         test_data = data[test_index]
         
+        # 適応度関数をラップしてtrain_dataを利用するように設定
+        def wrapped_evaluate(individual):
+            return evaluate(individual, train_data)
+        
+        # DEAPのtoolboxにラップした評価関数を登録
+        toolbox.register("evaluate", wrapped_evaluate)
+
         # 遺伝的アルゴリズムのパラメータ
         population = toolbox.population(n=50)
         ngen = 40
         cxpb = 0.5
         mutpb = 0.2
 
-        # 適応度の評価関数をtrain_dataで実行するように変更
-        def evaluate_individual(individual):
-            return toolbox.evaluate(individual, train_data)
-
         # 遺伝的アルゴリズムのセットアップ
         for ind in population:
-            ind.fitness.values = evaluate_individual(ind)
+            ind.fitness.values = toolbox.evaluate(ind)
 
         # アルゴリズムの実行
         algorithms.eaSimple(population, toolbox, cxpb, mutpb, ngen, verbose=True)
 
         # 最適な解を取得し、test_dataで評価
         best_individual = tools.selBest(population, k=1)[0]
-        best_fitness = toolbox.evaluate(best_individual, test_data)[0]
+        best_fitness = evaluate(best_individual, test_data)[0]
         best_individuals.append((best_individual, best_fitness))
+
+        # evaluate関数の登録を解除
+        del toolbox.evaluate
 
     return best_individuals
 
