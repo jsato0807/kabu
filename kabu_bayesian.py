895437b06eafd0036692ebef0625220b0e89f71f
jsato0807 <63589838+jsato0807@users.noreply.github.com>
2024-09-28 10:37:51 +0900

    modified to the newest version of Swapcalculator and traripi_backtest and added X_test to arg of objective_function

--- kabu_bayesian.py
+++ kabu_bayesian.py
@@ -5,9 +5,10 @@ from skopt.space import Real, Integer, Categorical
 from mpl_toolkits.mplot3d import Axes3D
 from skopt.utils import use_named_args
 from sklearn.model_selection import KFold
-from kabu_backtest import traripi_backtest, data, initial_funds, grid_start, grid_end, entry_intervals, total_thresholds, strategies
+from kabu_backtest import traripi_backtest, fetch_currency_data, initial_funds, grid_start, grid_end, entry_intervals, total_thresholds, strategies
 import numpy as np
 from kabu_swap import get_html, parse_swap_points, rename_swap_points, SwapCalculator
+from datetime import datetime
 
 url = 'https://fx.minkabu.jp/hikaku/moneysquare/spreadswap.html'
 html = get_html(url)
@@ -15,7 +16,13 @@ html = get_html(url)
 swap_points = parse_swap_points(html)
 swap_points = rename_swap_points(swap_points)
 #print(swap_points)
-calculator = SwapCalculator(swap_points)
+
+pair = 'USDJPY=X'
+interval="1d"
+end_date = datetime.strptime("2024-01-01","%Y-%m-%d")#datetime.now() - timedelta(days=7)
+start_date = datetime.strptime("2010-01-01","%Y-%m-%d")#datetime.now() - timedelta(days=14)
+data = fetch_currency_data(pair, start_date, end_date,interval)
+calculator = SwapCalculator(swap_points,pair)
 
 
 # パラメータ空間の定義
@@ -35,11 +42,11 @@ total_threshold = total_thresholds[0]
 
 
 # 交差検証を含む目的関数の定義
-def objective_function(X_train_cv):
+def objective_function(X_train_cv,X_test):
     @use_named_args(space)
     def inner_objective_function(num_trap, profit_width, order_size, strategy, density):
         # トレードバックテストの実行
-        effective_margin, _, _, _, _, _, _, _, _, _ = traripi_backtest(
+        effective_margin, _, _, _, _, _, _, _, _, _, _, _ = traripi_backtest(
             calculator, X_train_cv, initial_funds, grid_start, grid_end, num_trap, profit_width, order_size,
             entry_interval, total_threshold, strategy, density
         )
@@ -56,20 +63,21 @@ def objective_function(X_train_cv):
     # 最適なパラメータでテスト
     best_params = result.x
     best_effective_margin = traripi_backtest(
-        calculator, X_test, initial_funds, grid_start, grid_end, *best_params
+        calculator, X_test, initial_funds, grid_start, grid_end, best_params[0],best_params[1],best_params[2],entry_interval,total_threshold,best_params[3],best_params[4]
     )[0]
     
     return best_effective_margin, best_params
 
 # データの準備
-kf = KFold(n_splits=10)  # 5-fold クロスバリデーション
+kf = KFold(n_splits=5)  # 5-fold クロスバリデーション
 results = []
 
 for fold, (train_index, test_index) in enumerate(kf.split(data)):
-    X_train_cv, X_test = data[train_index], data[test_index]
+    X_train_cv, X_test = data.iloc[train_index], data.iloc[test_index]
+
     
     # 交差検証内でベイズ最適化を実行
-    margin, params = objective_function(X_train_cv)
+    margin, params = objective_function(X_train_cv,X_test)
     results.append({
         'fold': fold + 1,
         'margin': margin,
