514a564e9656826caaa2a8e3d8fef0e322128d63
jsato0807 <63589838+jsato0807@users.noreply.github.com>
2024-11-04 11:20:59 +0900

    modified to download from loacal directory instead of google drive and added coutry_name.replace and .date()to fix filename

--- kabu_bis_intrestrate.py
+++ kabu_bis_intrestrate.py
@@ -5,16 +5,15 @@ import gdown
 #のexportをクリックし、Bulk downloadsを選択したあと、Centrarl bank policy rates(CSV, flat)を選択するとデータを獲得可能
 
 
-filename = "WS_CBPOL_csv_flat.csv"
-file_id = "1q74UoB3Oby_1Ormlg-wny9n8ohG7awpR"
-url = f"https://drive.google.com/uc?id={file_id}"
+def filter_country_data(country_name: str, start_date: str, end_date: str) -> pd.DataFrame:
+    filename = "WS_CBPOL_csv_flat.csv"
+    file_id = "1q74UoB3Oby_1Ormlg-wny9n8ohG7awpR"
+    url = f"https://drive.google.com/uc?id={file_id}"
 
-# CSVファイルを読み込む
-# gdown.download(url, filename, quiet=False)
+    # CSVファイルを読み込む
+    # gdown.download(url, filename, quiet=False)
 
-data = pd.read_csv(filename, sep=',', low_memory=False)
-
-def filter_country_data(data: pd.DataFrame, country_name: str, start_date: str, end_date: str) -> pd.DataFrame:
+    data = pd.read_csv(f"./csv_dir/{filename}", sep=',', low_memory=False)
     """
     指定した国のデータをフィルタリングし、日付と観測値のペアを取得する関数。
 
@@ -28,6 +27,8 @@ def filter_country_data(data: pd.DataFrame, country_name: str, start_date: str, 
         pd.DataFrame: 日付と観測値のペアを含むデータフレーム
     """
     # 'REF_AREA:Reference area'から国名を含む行をフィルタリング
+    if "_" in country_name:
+        country_name = country_name.replace("_", " ")
     country_data = data[data['REF_AREA:Reference area'].str.endswith(country_name)].copy()  # コピーを作成
 
     # 日付を変換し、無効な日付を除外
@@ -40,14 +41,20 @@ def filter_country_data(data: pd.DataFrame, country_name: str, start_date: str, 
 
     # 必要な列のみを選択
     result_data = country_data[['TIME_PERIOD:Time period or range', 'OBS_VALUE:Observation Value']]
+
+    start_date = start_date.date()
+    end_date = end_date.date()
+    country_name = country_name.replace(" ","_")
+    result_data.to_csv(f"./csv_dir/kabu_bis_intrestrate_{country_name}_from{start_date}_to{end_date}.csv")
     
     return result_data
 
-# 任意の国名（例: 'Japan'）
-country_name = 'Japan'  # ここを任意の国に変更
-# 任意の日付範囲
-start_date = pd.to_datetime('1996-01-01')  # ここを変更
-end_date = pd.to_datetime('2000-12-31')    # ここを変更
-japan_data = filter_country_data(data, country_name, start_date, end_date)
+if __name__ == "__main__":
+    # 任意の国名（例: 'Japan'）
+    country_name = 'United States'  # ここを任意の国に変更
+    # 任意の日付範囲
+    start_date = pd.to_datetime('1996-01-01')  # ここを変更
+    end_date = pd.to_datetime('2000-12-31')    # ここを変更
+    japan_data = filter_country_data(country_name, start_date, end_date)
 
-print(japan_data)
+    print(japan_data)
